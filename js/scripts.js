0007a6a7be8cd837cba8af699b886efbdd326225
Jennifer Marks and Sophia Fujiki <sophia@>
2019-03-04 18:25:48 -0800

    Combined true statements

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,8 @@
 // Business Logic
 
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
-     return true;
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
    } else {
      return false;
    }
